d57265baeab1359361ffbf215866f97dd5e442ed
afryingpan <christopher.gomez.432@my.csun.edu>
2019-09-26 02:36:56 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/threejs-tilemap/precache-manifest.1724fa973079b90f4beebfd4161a5992.js"
+  "/threejs-tilemap/precache-manifest.85b6ab94d49acfc2d96b0c24a418ae13.js"
 );
 
 self.addEventListener('message', (event) => {
