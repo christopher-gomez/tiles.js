bd0aec5cc9646da2fa0606cb17d7fc0762cc0df8
afryingpan <christopher.gomez.432@my.csun.edu>
2019-10-08 10:27:48 -0700

    doc export order currently matters

--- src/Components/Docs/markdown/ref.js
+++ src/Components/Docs/markdown/ref.js
@@ -1,6 +1,6 @@
 export { default as TOC } from './toc.md';
 export { default as Intro } from './intro.md';
 export { default as Grid } from './grid.md';
-export { default as Interfaces } from './interfaces.md';
 export { default as Map } from './map.md';
 export { default as View } from './view.md';
+export { default as Interfaces } from './interfaces.md';
