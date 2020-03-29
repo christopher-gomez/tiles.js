a1579bee8ef1ee01da538fb363a36e4f278f67d8
Christopher Gomez <c.gomez3644@gmail.com>
2020-03-29 15:49:29 -0700

    Babale/Webpack build pipeline fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
